651908a464bec5fd130b2be88373c1b9aa0492d5
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2023-07-27 12:37:10 +0400

    added mappings and types for fundme contract

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,4 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract FundMe {}
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
+
+error FundMe__NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
+    address private immutable i_owner;
+    address[] private s_funders;
+    mapping(address => uint256) private s_addressToAmountFunded;
+    AggregatorV3Interface private s_priceFeed;
+
+}
