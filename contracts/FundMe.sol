57441d5428790bb6241d6f472dc7d5ed10b311be
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2023-07-29 13:47:12 +0400

    added fund function

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -26,4 +26,13 @@ contract FundMe {
         _;
     }
 
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+            "You need to spend more ETH"
+        );
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+    }
+
 }
