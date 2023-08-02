f48e19bedc76823f529244482281415aaad1e98f
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2023-08-02 11:47:13 +0400

    added deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,37 @@ const { networkConfig } = require("../helper-hardhat-config");
 const DECIMALS = "8";
 const INITIAL_PRICE = "200000000000";
 
-async function main() {}
+async function main() {
+  await deployFundMe();
+}
+
+async function deployFundMe() {
+  const chainId = network.config.chainId;
+  const accounts = await ethers.getSigners();
+  const deployer = accounts[0];
+
+  let mockPriceFeedAddress;
+  if (chainId == 31337) {
+    const mockPriceFeedFactory = await ethers.getContractFactory(
+      "MockV3Aggregator"
+    );
+    mockPriceFeed = await mockPriceFeedFactory
+      .connect(deployer)
+      .deploy(DECIMALS, INITIAL_PRICE);
+    await mockPriceFeed.deployed();
+    mockPriceFeedAddress = mockPriceFeed.address;
+  } else {
+    mockPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+  }
+  console.log("Deploying, please wait...");
+  const fundMeFacotry = await ethers.getContractFactory("FundMe");
+  const fundMe = await fundMeFacotry
+    .connect(deployer)
+    .deploy(mockPriceFeedAddress);
+  await fundMe.deployed();
+  console.log(`Deployed FundMe to ${fundMe.address}`);
+  return fundMe;
+}
 
 main().catch((error) => {
   console.error(error);
